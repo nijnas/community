a89a6c45bb4d35fdca821cfdd97d08255527b51c
nijnas <jibolun888@gmail.com>
2022-03-22 21:29:46 +0800

    更新

--- src/main/java/cn/hdu/community/entity/Article.java
+++ src/main/java/cn/hdu/community/entity/Article.java
@@ -1,12 +1,10 @@
 package cn.hdu.community.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.Instant;
 import java.util.Date;
 
 @Entity
